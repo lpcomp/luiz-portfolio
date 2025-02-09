2c8902373384ede65ae973f8fc801ffd33b3c9f9
Luiz Paulo Baçal de Vasconcelos <luizv993@gmail.com>
2025-02-09 18:09:55 -0300

    feat: lint

--- src/lib/utils/skills.tsx
+++ src/lib/utils/skills.tsx
@@ -1,41 +1,43 @@
 import Data from '@/lib/mocks/skills.json';
 
 type SkillType = {
-    title: string;
-    skills: Array<string>;
+  title: string;
+  skills: Array<string>;
 };
 
 interface Skills {
-    languages: SkillType,
-    frontEndLibs: SkillType,
-    frontEndTools: SkillType,
-    mobileDevelopment: SkillType,
-    backEnd: SkillType,
-    projectManagement: SkillType,
-    projectVersioning: SkillType,
-    others: SkillType
+  languages: SkillType;
+  frontEndLibs: SkillType;
+  frontEndTools: SkillType;
+  mobileDevelopment: SkillType;
+  backEnd: SkillType;
+  projectManagement: SkillType;
+  projectVersioning: SkillType;
+  others: SkillType;
 }
 
 const handleSkills = () => {
-    const skillsData: Skills = Data;
+  const skillsData: Skills = Data;
 
-    return (
-        <>          
-            {Object.keys(skillsData).map((category) => {
-              const skillCategory = skillsData[category as keyof Skills];
-              return (
-                <section key={category}>
-                  <h5 aria-label={category}>{skillCategory.title}</h5>
-                  <ul>
-                    {skillCategory.skills.map((skill, index) => (
-                      <li aria-label={skill} key={index}>{skill}</li>
-                    ))}
-                  </ul>
-                </section>
-              );
-            })}
-        </>
-    );
-}
+  return (
+    <>
+      {Object.keys(skillsData).map((category) => {
+        const skillCategory = skillsData[category as keyof Skills];
+        return (
+          <section key={category}>
+            <h5 aria-label={category}>{skillCategory.title}</h5>
+            <ul>
+              {skillCategory.skills.map((skill, index) => (
+                <li aria-label={skill} key={index}>
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </section>
+        );
+      })}
+    </>
+  );
+};
 
 export default handleSkills;
